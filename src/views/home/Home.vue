d3dfa80ec29d463f65ced07b3ef9e421344d0040
kuangw <kuangw@qq.com>
2020-05-31 18:35:52 +0800

    5.31更新

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,42 @@
 <template>
   <div>
-    <h2>首页</h2>
+    <nav-bar class="navBar">
+      <div slot="center"> 
+        首页
+      </div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+// 引入入组件
+import NavBar from 'components/common/navbar/NavBar'
+// 映入网络请求模块
+import {getHomeMultiData} from 'network/home'
+
 export default {
-  name:'Home'
+  name:'Home',
+  data() {
+    return {
+      result:null
+    }
+  },
+  components:{
+    NavBar
+  },
+  created() {
+    getHomeMultiData().then(res=>{
+      this.result= res
+    })
+  
+  },
 }
 </script>
 
 <style>
-
+.navBar{
+  background-color: var(--color-tint);
+  color: white;
+  font-size: 18px;
+}
 </style>
